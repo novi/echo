735fc988dfb978b60632ef06e1ed4895f60d0a60
Yusuke Ito <novi.mad@gmail.com>
2016-03-28 18:00:31 +0900

    remove server

--- Package.swift
+++ Package.swift
@@ -1,15 +1,7 @@
 import PackageDescription
 
-#if os(Linux)
-let libuvURL = "https://github.com/elliottminns/libuv-linux"
-#else
-let libuvURL = "https://github.com/elliottminns/libuv-osx"
-#endif
-
 let package = Package(
     name: "Echo",
     dependencies: [
-        .Package(url: libuvURL,
-                 majorVersion: 0)
     ]
 )
